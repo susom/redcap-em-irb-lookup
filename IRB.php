743efc776ea90d6ab36bc5a9cf00b0a2d5258014
Andy Martin <andy123@stanford.edu>
2018-05-15 12:14:25 -0700

    initial version

---
+++ IRB.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Stanford\IRB;
+
+Use ExternalModules\ExternalModules;
+Use ExternalModules\AbstractExternalModule;
+
+class IRB extends AbstractExternalModule
+{
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+The IRB Lookup class is a helper EM for accessing the IRB
+
+To use the IRB lookup class in your external module or plugin you will want to:
+
+```$xslt
+
+
+```
